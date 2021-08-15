f6c955e78fcc52c98e73908aed4afb4b98c95694
Mihndim Julius <mihndim2016@gmail.com>
2021-08-15 17:54:50 -0400

    Add todoItem component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import TodosList from "./TodoList";
 class TodoContainer extends React.Component {
   state = {
     todos: [
@@ -21,11 +22,7 @@ class TodoContainer extends React.Component {
    };
   render() {
     return (
-      <ul>
-      {this.state.todos.map(todo => (
-        <li>{todo.title}</li>
-      ))}
-    </ul>
+    <TodosList todos={this.state.todos} />
     )
   }
 }
