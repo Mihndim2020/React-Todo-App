64736b2d50b2bd6dfc9e62646cfe58056dc124f1
Mihndim Julius <mihndim2016@gmail.com>
2021-08-16 13:55:26 -0400

    Add css file

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react"
 
 class InputTodo extends Component {
+
+  state = {
+    title: ""
+  };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title)
+      this.setState({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    }
+  }
+  
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Add Todo..." />
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Add todo..." 
+        onChange={this.onChange}
+        value={this.state.title}
+        name="title" />
         <button>Submit</button>
       </form>
     )
