b87df0e3c8e9e7062edf8d0c81124a03e0138c5c
Mihndim Julius <mihndim2016@gmail.com>
2021-08-17 04:56:29 -0400

    Persist todo data to local storage using life cycle methods

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,24 +5,8 @@ import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
-   };
+    todos: [],
+  }
    handleChange = (id) => {
     this.setState(prevState => ({
       todos: prevState.todos.map(todo => {
@@ -66,6 +50,23 @@ class TodoContainer extends React.Component {
         return todo
       }),
     })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  }
+
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
   }
 
   render() {
